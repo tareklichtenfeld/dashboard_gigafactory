b86590fcb1e03d8260abeb5f2f32fbcae316b0c6
tareklichtenfeld <110313535+tareklichtenfeld@users.noreply.github.com>
2024-08-19 14:59:48 +0200

    Add files via upload

--- dashboard_gigafactory.py
+++ dashboard_gigafactory.py
@@ -386,10 +386,7 @@ cop_kkm = 6.1
 
 
 
-# Data-----------------------------------------------------------------------------------
-seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
-stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
-
+#--------------------DATA VISUALIZER-----------------------------------------------------------------------------------
 # Row A
 a1, a2, a3 = st.columns(3)
 a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
@@ -404,20 +401,3 @@ b3.metric("Nutz-Kältelast der Gigafactory",f"{round((RuT_GWh_k_nutz+Prozess_Kae
 b4.metric("End-Kältelast der Gigafactory",f"{round(RuT_GWh_k_end+(strom_eert(eer_avg, Prozess_Kaeltelast(production_capacity))),2)} GWh/a")
 
 # Row C
-c1, c2 = st.columns((7,3))
-with c1:
-    st.markdown('### Heatmap')
-    plost.time_hist(
-    data=seattle_weather,
-    date='date',
-    x_unit='week',
-    y_unit='day',
-    color='temp_max',
-    aggregate='median',
-    legend=None)
-with c2:
-    st.markdown('### Bar chart')
-    plost.donut_chart(
-        data=stocks,
-        theta='q2',
-        color='company')
